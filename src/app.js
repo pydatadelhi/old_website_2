4bde47b6d975097f45d47ddad4506a5dd747ffe2
ramantehlan <ramantehlan@gmail.com>
2019-05-17 15:58:19 +0530

    router update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// False while in development
 Vue.config.productionTip = false
 
 new Vue({
--- src/router.js
+++ src/router.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // Page content
-import Page1 from '@/components/Page1'
 import Home from '@/components/Home'
+import Conference from '@/components/Conference'
+import Archive from '@/components/Archive'
+import Blog from '@/components/Blog'
+import Sponsors from '@/components/Sponsors'
+import Volunteer from '@/components/Volunteer'
+import News from '@/components/News'
+import Team from '@/components/Team'
+import Contact from '@/components/Contact'
 
 // Fallback page
 import PageNotFound from '@/components/PageNotFound'
@@ -18,9 +25,44 @@ export default new Router({
       component: Home
     },
     {
-      path: '/page-1',
-      name: 'Page 1',
-      component: Page1
+      path: '/conference',
+      name: 'Conference',
+      component: Conference
+    },
+    {
+      path: '/archive',
+      name: 'Archive',
+      component: Archive
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
+    },
+    {
+      path: '/sponsors',
+      name: 'Sponsors',
+      component: Sponsors
+    },
+    {
+      path: '/volunteer',
+      name: 'Volunteer',
+      component: Volunteer
+    },
+    {
+      path: '/news',
+      name: 'News',
+      component: News
+    },
+    {
+      path: '/team',
+      name: 'Team',
+      component: Team
+    },
+    {
+      path: '/Contact',
+      name: 'Contact',
+      component: Contact
     },
     {
       path: '**',
