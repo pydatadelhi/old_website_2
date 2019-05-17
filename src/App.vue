f6bc0573573e721c5e2e72b07f9b67f2414e2122
ramantehlan <ramantehlan@gmail.com>
2019-05-17 22:28:59 +0530

    create menu

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(link, index) in links" :key="index">
-        <router-link :to="link.to">{{ link.name }}</router-link>
-      </li>
-    </ul>
+
+    <div id="top">
+
+      <div id="top-menu">
+        <ul class="menu">
+          <li v-for="(link, index) in links" :key="index">
+            <router-link :to="link.to"><div>{{ link.name }}</div></router-link>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
     <main>
       <router-view/>
     </main>
+
   </div>
 </template>
 
-<style scoped>
-  ul {
-    list-style: none;
-    display: flex;
-    padding: 0;
-  }
-  li {
-    padding: 5px 15px 5px 0;
-  }
-  li a {
-    text-decoration: none;
-    color: black;
-  }
-  li a:hover {
-    color: #404040;
-  }
-</style>
+
 
 
 <script>
