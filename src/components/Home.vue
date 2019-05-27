c8a0b0d5f3f4b6c52b28e3550e0cb883c3d8cdf8
neerajp99 <neerajp1999@gmail.com>
2019-05-27 18:36:10 +0530

    initial home route

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,100 @@
 <template>
   <div>
-    <h1>Home</h1>
+  <!-- Landing page goes here -->
+    <div class = "home_first">
+      <div class = "home_first_block">
+        <h1 class = "home_heading">PyData Delhi</h1>
+        <br>
+        <h2 class = "home_subheading"> A community of users and developers of the Python Scientific Stack in Delhi.</h2>
+        <br>
+        <button class = "home_meetup_button">join the meetup</button>
+        <br>
+      </div>
+  	</div>
+    <hr>
+
+  <!-- Pydata page goes here -->
+  <div class = "home_pydata">
+   <hr class = "home_pydata_hr">
+   <h1 class = "home_pydata_heading">
+      PyData
+   </h1>
+   <br>
+   <div class="home_pydata_left">
+      <p class = "home_pydata_left_para">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <br>
+      <button class = "home_pydata_left_button">Website</button>
+      <br>
+      <ul class = "home_pydata_social">
+         <li><a href="#"><i class="fab fa-twitter fa-3x"></i></a></li>
+         <li><a href="#"><i class="fab fa-youtube fa-3x"></i></a></li>
+         <li><a href="#"><i class="fab fa-github fa-3x"></i></a></li>
+         <li><a href="https://www.meetup.com/pro/pydata/"><i class="fab fa-meetup fa-3x"></i></a></li>
+      </ul>
+   </div>
+   <div class="home_pydata_right">
+      <div class="svg_1">
+         <svg xmlns="http://www.w3.org/2000/svg" width="459" height="239" viewBox="0 0 459 239" fill="#f77c18">
+            <path class="blob-shape" d="M1.53561257,0 L458.12641,0 C461.773868,71.7384575 420.478337,183.060211 311.475162,224.585557 C194.808755,269.032251 89.8950627,199.485285 47.8554028,150.197559 C11.1268941,107.139844 -2.14235785,50.799793 1.53561316,-8.15533587e-06 L1.53561257,0 Z" transform="translate(-968.000000, -3455.000000) translate(968.000000, 3455.000000)"></path>
+         </svg>
+      </div>
+   </div>
+</div>
+<!-- Pydata Delhi page goes here -->
+
+<div class = "home_pydata_delhi">
+  <h1 class = "home_pydata_delhi_heading">PyData <span>Delhi</span> </h1>
+
+  <div class="home_pydata_delhi_left">
+    <p class = "home_pydata_delhi_left_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doconsectetur adipisicing elit, sed do consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mieiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <br>
+    <ul class = "home_pydata_delhi_left_social">
+       <li><a href="#"><i class="fab fa-twitter fa-3x"></i></a></li>
+       <li><a href="#"><i class="fab fa-youtube fa-3x"></i></a></li>
+       <li><a href="#"><i class="fab fa-github fa-3x"></i></a></li>
+       <li><a href="https://www.meetup.com/pro/pydata/"><i class="fab fa-meetup fa-3x"></i></a></li>
+    </ul>
+    <br>
+    <button class = "home_pydata_delhi_left_button1" id = "home_pydata_delhi_left_button">Conference</button>
+    <button class = "home_pydata_delhi_left_button2" id = "home_pydata_delhi_left_button">Meetup</button>
+
+  </div>
+	<div class="home_pydata_delhi_right">
+    <img src="../../static/tmp_desing/other/pydd.jpeg" class = "home_pydata_delhi_right_image"/>
+  </div>
+
+</div>
+<!-- CFP part goes here -->
+<div class="home_pydata_cfp">
+   <h1 class = "home_pydata_cfp_heading">Want to speak at a PyData Delhi event?</h1>
+   <hr>
+   <br>
+   <ul class = "home_pydata_cfp_list">
+      <li class = "home_pydata_cfp_list_data">
+         <div class = "home_pydata_cfp_list_div">
+            <img src="../../static/tmp_desing/other/speaker.png" class = "home_pydata_cfp_image"/>
+            <h3 class = "">Talks</h3>
+            <br>
+            <p class = "">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+      </li>
+      <li class = "home_pydata_cfp_list_data">
+         <div class = "home_pydata_cfp_list_div">
+            <img src="../../static/tmp_desing/other/workshop.png" class = "home_pydata_cfp_image"/>
+            <h3 class = "">Workshops</h3>
+            <br>
+            <p class = "">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+      </li>
+   </ul>
+   <br>
+   <button>Submit an issue here</button>
+</div>
+
+
   </div>
 </template>
 
