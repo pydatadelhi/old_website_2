c1cd938ace048e1c0ad39423b030483496035cab
khushal <vyas.khushal1999@gmail.com>
2019-05-30 02:00:19 +0530

    added media queries for responsiveness

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -10,77 +10,73 @@
     
       <!-- CONTACT PAGE LEFT --> 
     
-    <form class="form-horizontal" role="form" method="post" action="contact.php">
-       
-      <div class="form-group">
-        <div class="col-sm-16">
-          <input type="text" class="form-control" id="name" placeholder="NAME" name="name" value="">
-        </div>
+  <form class="form-horizontal" role="form" method="post" action="contact.php">
+      
+    <div class="form-group">
+      <div class="col-sm-16">
+        <input type="text" class="form-control" id="name" placeholder="NAME" name="name" value="">
       </div>
-      <br>
-      <div class="form-group">
-        <div class="col-sm-16">
-          <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email" value="">
-        </div>
+    </div>
+    <br>
+    <div class="form-group">
+      <div class="col-sm-16">
+        <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email" value="">
       </div>
-      <br>
-      <textarea id="textarea" rows="10" cols="32" placeholder="MESSAGE" name="message"></textarea>
-      
-      <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
-        <div class="button">
-          <i class="fa fa-paper-plane" id="plane"></i><span class="send-text">SEND</span>
-        </div>
-      
-      </button>
-      
-    </form>
+    </div>
+    <br>
+    <textarea id="textarea" rows="10" cols="32" placeholder="MESSAGE" name="message"></textarea>
+    
+    <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+      <div class="button">
+        <i class="fa fa-paper-plane" id="plane"></i><span class="send-text">SEND</span>
+      </div>
+    </button>
+    
+  </form>
     
       <!-- CONTACT PAGE RIGHT --> 
       <div class="direct-contact-container">
 
-        <ul class="contact-list">
-          <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place">New Delhi | IND</span></i></li>
-          
-          <li class="list-item"><i class="fa fa-user-circle-o fa-2x"><span class="contact-text phone"><a href="mailto:svsanketverma5@gmail.com" title="Organiser">svsanketverma5@gmail.com </a></span></i></li>
-          
-          <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:Delhi@pydata.org" title="Send me an email">Delhi@pydata.org@gmail.com</a></span></i></li>
-          
-        </ul>
+      <ul class="contact-list">
+        <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place">New Delhi | IND</span></i></li>
+        
+        <li class="list-item"><i class="fa fa-user-circle-o fa-2x"><span class="contact-text phone"><a href="mailto:svsanketverma5@gmail.com" title="Organiser">svsanketverma5@gmail.com </a></span></i></li>
+        
+        <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:Delhi@pydata.org" title="Send me an email">Delhi@pydata.org@gmail.com</a></span></i></li>
+      </ul>
 
-        <hr>
-        <ul class="social-media-list">
-          <li><a href="https://github.com/pydatadelhi" target="_blank" class="contact-icon">
-            <i class="fa fa-github" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://twitter.com/PyDataDelhi" target="_blank" class="contact-icon">
-            <i class="fa fa-twitter" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://instagram.com/pydatadelhi?igshid=qe7lft8thwl7" target="_blank" class="contact-icon">
-            <i class="fa fa-instagram" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://www.meetup.com/pro/pydata/" target="_blank" class="contact-icon">
-            <i class="fa fa-meetup" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://www.facebook.com/pydatadelhi" target="_blank" class="contact-icon">
-            <i class="fa fa-facebook" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://www.youtube.com/channel/UCz_xSLNdtnkDsVTuYuLprMg" target="_blank" class="contact-icon">
-            <i class="fa fa-youtube" aria-hidden="true"></i></a>
-          </li>
-          <li><a href="https://bit.do/joinpydd" target="_blank" class="contact-icon">
-            <i class="fa fa-telegram" aria-hidden="true"></i></a>
-          </li>
-        </ul>
-        <hr>
+      <hr>
+      <ul class="social-media-list">
+        <li><a href="https://github.com/pydatadelhi" target="_blank" class="contact-icon">
+          <i class="fa fa-github" aria-hidden="true"></i></a>
+        </li>
+        <li><a href="https://twitter.com/PyDataDelhi" target="_blank" class="contact-icon">
+          <i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </li>
+        <li><a href="https://instagram.com/pydatadelhi?igshid=qe7lft8thwl7" target="_blank" class="contact-icon">
+          <i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </li>
+        <li><a href="https://www.meetup.com/pro/pydata/" target="_blank" class="contact-icon">
+          <i class="fa fa-meetup" aria-hidden="true"></i></a>
+        </li>
+        <li><a href="https://www.facebook.com/pydatadelhi" target="_blank" class="contact-icon">
+          <i class="fa fa-facebook" aria-hidden="true"></i></a>
+        </li>
+        <li><a href="https://www.youtube.com/channel/UCz_xSLNdtnkDsVTuYuLprMg" target="_blank" class="contact-icon">
+          <i class="fa fa-youtube" aria-hidden="true"></i></a>
+        </li>
+        <li><a href="https://bit.do/joinpydd" target="_blank" class="contact-icon">
+          <i class="fa fa-telegram" aria-hidden="true"></i></a>
+        </li>
+      </ul>
+      <hr>
 
-        <div class="copyright">&copy; PyData Delhi 2019</div>
+     <div class="copyright">&copy; PyData Delhi 2019</div>
 
-      </div>
-    
-  </div>
-  
-</section>  
-  
+     </div>
+    </div>
+  </section> 
+
   </div>
 </template>
 
@@ -282,6 +278,80 @@
 
     hr {
       border-color: rgba(255,255,255,.8); ;
+    }
+
+    /*Media Queries for making page responsive*/
+    @media screen and (max-width: 760px) {
+        #contact {
+            height: 1000px;
+        }
+      .section-header {
+        font-size: 65px;
+      }
+      .direct-contact-container, .form-horizontal {
+        float: none;
+        margin: 10px auto;
+      }  
+      .direct-contact-container {
+        margin-top: 60px;
+        max-width: 300px;
+      }    
+      .social-media-list li {
+        height: 60px;
+        width: 60px;
+        line-height: 60px;
+      }
+      .social-media-list li:after {
+        width: 60px;
+        height: 60px;
+        line-height: 60px;
+      }
+    }
+
+    @media screen and (max-width: 569px) {
+      #contact {
+        height: 1200px;
+        }
+      .section-header{
+        font-size: 50px;
+      }
+      .direct-contact-container, .form-wrapper {
+        float: none;
+        margin: 0 auto;
+      }  
+      .form-control, textarea {
+        max-width: 340px;
+        margin: 0 auto;
+      }
+      
+      .name, .email, textarea {
+        width: 280px;
+      } 
+      .direct-contact-container {
+        margin-top: 60px;
+        max-width: 280px;
+      }  
+      .social-media-list {
+        left: 0;
+      }
+      .social-media-list li {
+        height: 55px;
+        width: 55px;
+        line-height: 55px;
+        font-size: 2rem;
+      }
+      .social-media-list li:after {
+        width: 55px;
+        height: 55px;
+        line-height: 55px;
+      }
+      
+    }
+
+    @media screen and (max-width: 410px) {
+      .send-button {
+        width: 99%;
+      }
     }
 
 </style>
